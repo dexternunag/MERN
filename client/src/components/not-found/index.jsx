6d108c1ef97cbc99a3b2570974e6bfb67951d733
dexternunag <nunagdexter@gmail.com>
2018-08-12 23:17:52 +0800

    Finish profiles and profile viewing

---
+++ client/src/components/not-found/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <h1 className="d-4">Page not found!</h1>
+      <p>Sorry, this page does not exists</p>
+    </div>
+  )
+}
---
+++ client/src/components/profiles/index.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import Spinner from '../common/Spinner'
+import { getProfiles } from '../../actions/profileActions'
+import ProfileItem from './ProfileItem'
+
+class Profiles extends Component {    
+    componentDidMount() {
+        this.props.getProfiles();
+    }
+
+    render() {
+        const { profiles, loading } = this.props.profile;
+        let profileItems;
+
+        if (profiles === null || loading) {
+            profileItems = <Spinner />;
+        } else {
+            if (profiles.length > 0) {
+                profileItems = profiles.map(profile => (
+                    <ProfileItem key={profile._id} profile={profile} />
+                ));
+            } else {
+                profileItems = <h4>No Profiles Found</h4>;
+            }
+        }
+
+        return (
+            <div className="profiles">
+                <div className="containter">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="display-4 text-center">Developer Profiles</div>
+                            <p className="lead text-center">
+                                Browse and connect with developers
+                            </p>
+                            {profileItems}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+Profiles.propTypes = {
+    getProfiles: PropTypes.func.isRequired
+}
+
+const mapToStateToProps = state => ({
+    profile: state.profile
+})
+
+export default connect(mapToStateToProps, {getProfiles})(Profiles);
